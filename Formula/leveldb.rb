c82f5e500f6a6b7ad908a5901b5697b6b060618d
vTorrent <vtorrent.crypto@gmail.com>
2019-05-28 01:23:54 +0700

    LevelDB 1.22

--- Formula/leveldb.rb
+++ Formula/leveldb.rb
@@ -1,38 +1,37 @@
 class Leveldb < Formula
   desc "Key-value storage library with ordered mapping"
   homepage "https://github.com/google/leveldb/"
-  url "https://github.com/google/leveldb/archive/v1.20.tar.gz"
-  sha256 "f5abe8b5b209c2f36560b75f32ce61412f39a2922f7045ae764a2c23335b6664"
+  url "https://github.com/google/leveldb/archive/1.22.tar.gz"
+  sha256 "55423cac9e3306f4a9502c738a001e4a339d1a38ffbee7572d4a07d5d63949b2"
 
   bottle do
+    root_url "https://linuxbrew.bintray.com/bottles"
     cellar :any
-    sha256 "429fe042688a6c7bab742fd73e518336b4c656af562797e8b3a08b21d4a5453b" => :sierra
-    sha256 "161dbb44dada171246c6c00efe96822621cfa366ce8057eac18ec464d20ce072" => :el_capitan
-    sha256 "556dd6f11381e5fa8685b79f73ebc602e83d0412af5d8c5f196abd4d2e12be6e" => :yosemite
+    sha256 "22e4a129bedd5030525f749a5b5ec978bf6da0a9b0625fe829da482a5ab85755" => :mojave
+    sha256 "b1cf697cad28caac418d2e0ef49bc90863f389402185d3cc0f1f7079516d02c2" => :high_sierra
+    sha256 "810dbeba5e3f7d72d4772b9eff4d9022a1240c0abc6235afbd343c199741e6f7" => :sierra
+    sha256 "debca309da693fdb3461c429d0f9116bb8970bd08833276a6400a280c458d8b7" => :x86_64_linux
   end
 
-  option "with-test", "Verify the build with make check"
-
+  depends_on "cmake" => :build
   depends_on "gperftools"
   depends_on "snappy"
 
   def install
-    system "make"
-    system "make", "check" if build.bottle? || build.with?("test")
+    mkdir "build" do
+      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=ON"
+      system "make", "install"
+      bin.install "leveldbutil"
 
-    include.install "include/leveldb"
-    include.install "helpers"
-    bin.install "out-static/leveldbutil"
-    lib.install "out-static/libleveldb.a"
-    lib.install "out-static/libmemenv.a"
-    lib.install "out-shared/libleveldb.dylib.1.20" => "libleveldb.1.20.dylib"
-    lib.install_symlink lib/"libleveldb.1.20.dylib" => "libleveldb.dylib"
-    lib.install_symlink lib/"libleveldb.1.20.dylib" => "libleveldb.1.dylib"
-    MachO::Tools.change_dylib_id("#{lib}/libleveldb.1.dylib", "#{lib}/libleveldb.1.20.dylib")
+      system "make", "clean"
+      system "cmake", "..", *std_cmake_args, "-DBUILD_SHARED_LIBS=OFF"
+      system "make"
+      lib.install "libleveldb.a"
+      lib.install "libmemenv.a"
+    end
   end
 
   test do
     assert_match "dump files", shell_output("#{bin}/leveldbutil 2>&1", 1)
   end
 end
-
